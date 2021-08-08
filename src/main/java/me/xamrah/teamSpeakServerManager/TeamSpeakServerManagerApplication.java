a99ef45ed027e78202e5866dca695e930db3169b
Evgeniy Khamrachev <xamrah93rus@mail.ru>
2021-08-08 22:33:18 +0300

    Initial commit

---
+++ src/main/java/me/xamrah/teamSpeakServerManager/TeamSpeakServerManagerApplication.java
@@ -0,0 +1,13 @@
+package me.xamrah.teamSpeakServerManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSpeakServerManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamSpeakServerManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/xamrah/teamSpeakServerManager/TeamSpeakServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.xamrah.teamSpeakServerManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamSpeakServerManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
